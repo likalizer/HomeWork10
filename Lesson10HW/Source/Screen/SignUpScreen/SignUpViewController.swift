456d9a37770a09e141a671d769f662931cfc87f9
likalizer <lika.lika1823@gmail.com>
2024-04-24 21:49:23 +0300

    Завдання 8.3 додано

--- Lesson10HW/Source/Screen/SignUpScreen/SignUpViewController.swift
+++ Lesson10HW/Source/Screen/SignUpScreen/SignUpViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var contentView: SignUpView!
     
     override func viewDidLoad() {
-      
-    }
-    
+        
+      super.viewDidLoad()
+                setupTextFieldDelegates()
+            }
 
-}
+            private func setupTextFieldDelegates() {
+                contentView.cardNumberTextField.delegate = self
+                contentView.cvvTextField.delegate = self
+            }
+
+    @objc(textField:shouldChangeCharactersInRange:replacementString:) func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+                let newText = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+                
+                if textField == contentView.cardNumberTextField {
+                    return newText.count <= 16
+                } else if textField == contentView.cvvTextField {
+                    return newText.count <= 3
+                }
+                
+                return true 
+            }
+        }
+
